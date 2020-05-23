7813cfe2bd7aa4dc1e37a84e0e700a3ba0acf1e2
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-23 14:47:02 -0300

    Add attributes to Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,11 @@ class Article < ApplicationRecord
 	# You can call this whatever you want
 	# has_one_attached :banana
 	has_one_attached :pdf
+
+	def Article.attributes
+		%i[
+			title
+			pdf
+		]
+	end
 end
